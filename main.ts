4b3d91ba51bbe4405a2aaef6473f0b8bd42c9ecc
Biggiehampster <124838796+Biggiehampster@users.noreply.github.com>
2024-01-12 09:11:03 -0600

    made it so the coin despawns when you touch it

--- main.ts
+++ main.ts
@@ -232,69 +232,11 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 3
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    animation.runImageAnimation(
-    gameend,
-    [img`
-        . . b b b b . . 
-        . b 5 5 5 5 b . 
-        b 5 d 3 3 d 5 b 
-        b 5 3 5 5 1 5 b 
-        c 5 3 5 5 1 d c 
-        c d d 1 1 d d c 
-        . f d d d d f . 
-        . . f f f f . . 
-        `,img`
-        . . b b b . . . 
-        . b 5 5 5 b . . 
-        b 5 d 3 d 5 b . 
-        b 5 3 5 1 5 b . 
-        c 5 3 5 1 d c . 
-        c 5 d 1 d d c . 
-        . f d d d f . . 
-        . . f f f . . . 
-        `,img`
-        . . . b b . . . 
-        . . b 5 5 b . . 
-        . b 5 d 1 5 b . 
-        . b 5 3 1 5 b . 
-        . c 5 3 1 d c . 
-        . c 5 1 d d c . 
-        . . f d d f . . 
-        . . . f f . . . 
-        `,img`
-        . . . b b . . . 
-        . . b 5 5 b . . 
-        . . b 1 1 b . . 
-        . . b 5 5 b . . 
-        . . b d d b . . 
-        . . c d d c . . 
-        . . c 3 3 c . . 
-        . . . f f . . . 
-        `,img`
-        . . . b b . . . 
-        . . b 5 5 b . . 
-        . b 5 1 d 5 b . 
-        . b 5 1 3 5 b . 
-        . c d 1 3 5 c . 
-        . c d d 1 5 c . 
-        . . f d d f . . 
-        . . . f f . . . 
-        `,img`
-        . . . b b b . . 
-        . . b 5 5 5 b . 
-        . b 5 d 3 d 5 b 
-        . b 5 1 5 3 5 b 
-        . c d 1 5 3 5 c 
-        . c d d 1 d 5 c 
-        . . f d d d f . 
-        . . . f f f . . 
-        `],
-    100,
-    false
-    )
+    sprites.destroy(gameend)
     info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(-1)
     game.gameOver(false)
 })
 let direction = 0
@@ -504,6 +446,66 @@ if (Mainplayer.vy == 0) {
     Mainplayer.vy = -150
 }
 gameend.setPosition(58, 0)
+animation.runImageAnimation(
+gameend,
+[img`
+    . . b b b b . . 
+    . b 5 5 5 5 b . 
+    b 5 d 3 3 d 5 b 
+    b 5 3 5 5 1 5 b 
+    c 5 3 5 5 1 d c 
+    c d d 1 1 d d c 
+    . f d d d d f . 
+    . . f f f f . . 
+    `,img`
+    . . b b b . . . 
+    . b 5 5 5 b . . 
+    b 5 d 3 d 5 b . 
+    b 5 3 5 1 5 b . 
+    c 5 3 5 1 d c . 
+    c 5 d 1 d d c . 
+    . f d d d f . . 
+    . . f f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . b 5 d 1 5 b . 
+    . b 5 3 1 5 b . 
+    . c 5 3 1 d c . 
+    . c 5 1 d d c . 
+    . . f d d f . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . . b 1 1 b . . 
+    . . b 5 5 b . . 
+    . . b d d b . . 
+    . . c d d c . . 
+    . . c 3 3 c . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b . . . 
+    . . b 5 5 b . . 
+    . b 5 1 d 5 b . 
+    . b 5 1 3 5 b . 
+    . c d 1 3 5 c . 
+    . c d d 1 5 c . 
+    . . f d d f . . 
+    . . . f f . . . 
+    `,img`
+    . . . b b b . . 
+    . . b 5 5 5 b . 
+    . b 5 d 3 d 5 b 
+    . b 5 1 5 3 5 b 
+    . c d 1 5 3 5 c 
+    . c d d 1 d 5 c 
+    . . f d d d f . 
+    . . . f f f . . 
+    `],
+100,
+true
+)
 game.onUpdate(function () {
     if (Mainplayer.isHittingTile(CollisionDirection.Bottom)) {
         candouble_jump = true
